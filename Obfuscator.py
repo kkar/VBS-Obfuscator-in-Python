50cfb7968f1655697c6fd44c5c2ba12739e60e5d
Konstantinos Karakatsoulis <kkar@users.noreply.github.com>
2015-06-04 15:26:12 +0300

    Added random capitalization
    Added random capitalization for string-based detection evasion

--- Obfuscator.py
+++ Obfuscator.py
@@ -12,8 +12,20 @@ if len(sys.argv) <> 3:
 #since we are not using it in obfuscation
 splitter = str(chr(42))
 
+#Randomly capitalize each character
+def randCapitalization(characters):
+	capicharacter = ""
+	for character in characters:
+		lowup = random.randrange(0,2)
+		if lowup == 0:
+			capicharacter += character.upper()
+		if lowup == 1:
+			capicharacter +=  character.lower()
+	return capicharacter
+
+
 #Random function names
-NUM_OF_CHARS = random.randrange(5, 20)
+NUM_OF_CHARS = random.randrange(5, 60)
 pld = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(NUM_OF_CHARS))
 array = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(NUM_OF_CHARS))
 temp = ''.join(random.choice(string.ascii_uppercase + string.ascii_lowercase) for _ in range(NUM_OF_CHARS))
@@ -32,7 +44,7 @@ def obfu(body):
 
 #Random mathematical expression decision
 def expr(char):
-	range = random.randrange(1, 10001)
+	range = random.randrange(100, 10001)
 	exp = random.randrange(0, 3)
 
 	if exp == 0:
@@ -50,13 +62,13 @@ clear_text_file = open(sys.argv[1], "r")
 obfuscated_file = open(sys.argv[2], "w")
 
 #Write to destination file
-obfuscated_file.write("Dim " + pld + ", " + array + ", " + temp + "\n")
-obfuscated_file.write(pld + " = " + chr(34) + obfu(clear_text_file.read()) + chr(34) + "\n")
-obfuscated_file.write(array + " = Split(" + pld + ", chr(eval(" + obfu(splitter) + ")))\n")
-obfuscated_file.write("for each " + x + " in " + array + "\n")
-obfuscated_file.write(temp + " = " + temp + " & chr(eval(" + x + "))\n")
-obfuscated_file.write("next\n")
-obfuscated_file.write("execute(" + temp + ")\n")
+obfuscated_file.write(randCapitalization("Dim " + pld + ", " + array + ", " + temp) + "\n")
+obfuscated_file.write(randCapitalization(pld + " = ") + chr(34) + obfu(clear_text_file.read()) + chr(34) + "\n")
+obfuscated_file.write(randCapitalization(array + " = Split(" + pld + ", chr(eval(") + obfu(splitter) + ")))\n")
+obfuscated_file.write(randCapitalization("for each " + x + " in " + array) + "\n")
+obfuscated_file.write(randCapitalization(temp + " = " + temp + " & chr(eval(" + x) + "))\n")
+obfuscated_file.write(randCapitalization("next") + "\n")
+obfuscated_file.write(randCapitalization("execute(" + temp) + ")\n")
 
 #Close file handles before exit
 clear_text_file.close()
